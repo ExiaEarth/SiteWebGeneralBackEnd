f94f0a394f05e8e0eb24561dd985d7e45e05dbef
Mist <marcus-le@hotmail.com>
2022-10-31 15:17:24 +0100

    Initiation du serveur

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv-flow').config();
+const express=require('express');
+const cors=require('cors');
+const{DB_CONNECTION,PORT}=process.env;
+
+require('express-async-errors');
+const mongoose=require('mongoose');
+const app=express();
+app.use(express.json())
+app.use(cors())
+app.use(express.static('Public'));
+const router=require('./Routes/index-routes');
+
+app.use(async(req,res,next)=>{await mongoose.connect(DB_CONNECTION)
+    console.log('Connection OK.')
+    next();
+});
+
+app.use('/api',router);
+app.listen(PORT,()=>{console.log(`Le serveur est sur le port:${PORT}`);});//
